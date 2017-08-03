ea463c3d10c647d0e5b40e0eea7bc201a81d3234
Derek Dominish <DDominish@users.noreply.github.com>
2017-08-03 14:33:42 +0930

    Added some descriptive material for review

--- daf/CountDownSemaphore.h
+++ daf/CountDownSemaphore.h
@@ -53,24 +53,39 @@ namespace DAF
         /** Constructor @a count >= 0 */
         CountDownSemaphore(int count);
 
-        /** Current count value */
+        /** Accessor to the current count value */
         int count(void) const;
 
+        /**
+        * If @a abstime == 0 then call acquire() directly for infinate time.
+        * Otherwise, block the acquiring thread until the count is 0 or
+        * until @a abstime times out, in which case -1 is returned
+        * with @c errno == @c ETIME.  Note that @a abstime is assumed to
+        * be in "absolute" rather than "relative" time.
+        */
         int acquire(const ACE_Time_Value * abstime = 0);
         int acquire(const ACE_Time_Value & abstime);
         int acquire(time_t msecs);
 
         int attempt(time_t msecs); // Backwards Compatability
 
+        /**
+        * Release (decrement) the current count by @a count and broadcast 
+        * to waiting acquirer's if 0. NOTE: underlying count will not be
+        * allowed to go negative.
+        */
         int release(int count = 1);
 
+        /**
+        * Allow public access to underlying Monitor capabilities
+        */
         using Monitor::waiters;
         using Monitor::interrupt;
         using Monitor::interrupted;
 
     private:
 
-        int count_;
+        int count_; // The current counter >= 0 always
     };
 
     inline
